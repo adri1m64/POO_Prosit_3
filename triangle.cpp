d9564a61a1437d7e136c6649759805b504646e38
Theo <theo.prieur1@viacesi.fr>
2025-11-18 15:47:16 +0100

    Triangle complet

--- triangle.cpp
+++ triangle.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <cmath>
+#include "math.h"
 #include "triangle.h"
 using namespace std;
 
 double Triangle::Aire(Point p1, Point p2, Point p3){
     //(base*hauteur)/2
     double a = p1.distance(p2);  // base
-    double numerator = Math.abs((p2.getX() - p1.getX()) * (p1.getY() - p3.getY()) - (p1.getX() - p3.getX()) * (p2.getY() - p1.getY()));
+    double numerator = abs((p2.getX() - p1.getX()) * (p1.getY() - p3.getY()) - (p1.getX() - p3.getX()) * (p2.getY() - p1.getY()));
     double h = numerator / a; 
     aire = (a * h) / 2;
     return aire;
