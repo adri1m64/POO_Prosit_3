fc4dc43e3fb272f3bfc91d755bb03afbeaedc7fd
sdurand <sarahdurand2007@gmail.com>
2025-11-18 15:05:10 +0100

    methods FormeGeometrique

--- FormeGeometrique.cpp
+++ FormeGeometrique.cpp
@@ -1,1 +1,12 @@
 #include <iostream>
+
+#include "FormeGeometrique.h"
+#include "Point.h"
+
+Point FormeGeometrique::getPosition(){
+    return position;
+}
+
+void FormeGeometrique::setPosition(Point pos){
+    position = pos;
+}
--- FormeGeometrique.h
+++ FormeGeometrique.h
@@ -1,1 +1,25 @@
+#ifndef FORMEGEOMETRIQUE_H
+#define FORMEGEOMETRIQUE_H
+
 #include <iostream>
+#include "Point.h"
+
+using namespace std;
+
+class FormeGeometrique{
+protected:
+    Point position;
+
+public:
+    FormeGeometrique(Point pos):position(pos) {};
+
+    Point getPosition();
+    void setPosition(Point pos);
+
+    double aire();
+    double perimetre();
+    void afficher();
+
+};
+
+#endif
